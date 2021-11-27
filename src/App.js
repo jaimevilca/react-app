5e04afc5693d141ffb8458f9170e1ef0a8b98a0f
Jaime Vilca <jaime_v@outlook.com>
2021-11-27 11:28:53 -0500

    Init app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Init</h1>
+    </div>
+  );
+}
+
+export default App;
